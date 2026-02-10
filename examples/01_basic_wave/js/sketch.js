8b14049984edd423979d990c209403885187c42d
Seb <108084902+seb-prjcts-be@users.noreply.github.com>
2026-02-10 13:11:49 +0100

    Add standalone examples

---
+++ examples/01_basic_wave/js/sketch.js
@@ -0,0 +1,23 @@
+const SELECT = 'classic sine';
+const SCALE = 80;
+const STEP = 4;
+const SPEED = 0.5;
+
+function setup() {
+  createCanvas(400, 200);
+  noFill();
+  stroke(20);
+}
+
+function draw() {
+  background(245);
+  beginShape();
+  for (let y = 0; y <= height; y += STEP) {
+    const x = width / 2 + Waves.wave(y + frameCount * SPEED, SELECT, null, {
+      axis: 'x',
+      scale: SCALE
+    });
+    vertex(x, y);
+  }
+  endShape();
+}
