89edb22a5c54762df2a6fe836592d7283bd3739f
Seb <108084902+seb-prjcts-be@users.noreply.github.com>
2026-02-18 12:53:51 +0100

    chore: commit old folder baseline snapshot

---
+++ start-from-v1.1.0-snapshot/examples/02_instance_mode/js/sketch.js
@@ -0,0 +1,26 @@
+const SELECT = 'classic sine';
+const AMPLITUDE = 80;
+const STEP = 4;
+const SPEED = 0.5;
+const SECONDS = 1.5;
+
+new p5(function (p) {
+  p.setup = function () {
+    p.createCanvas(400, 200);
+    p.noFill();
+    p.stroke(20);
+  };
+
+  p.draw = function () {
+    p.background(245);
+    p.beginShape();
+    for (let y = 0; y <= p.height; y += STEP) {
+      const x = p.width / 2 + p.waves(y + p.frameCount * SPEED, SELECT, SECONDS, {
+        axis: 'x',
+        amplitude: AMPLITUDE
+      });
+      p.vertex(x, y);
+    }
+    p.endShape();
+  };
+});
